63d11c9f54644a7a86bf8fc4b7d257b6a54d7ef8
Polina Bakova <bakovapolina@mail.ru>
2020-09-15 01:16:23 +0300

    Личный проект: большие перемены (Часть 2)

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export const getCurrentDate = () => {
   const currentDate = new Date();
 
@@ -14,6 +16,42 @@ export const humanizeYearMonthDay = (dueDate) => {
 
 export const humanizeFull = (dueDate) => {
   return dueDate.toLocaleString(`en-GB`, {year: `2-digit`, month: `numeric`, day: `numeric`, hour: `numeric`, minute: `numeric`});
+};
+
+export const formatHumanizeDate = (dueDate) => {
+  if (!(dueDate instanceof Date)) {
+    return ``;
+  }
+  return moment(dueDate).format(`HH:mm`);
+};
+
+export const formatHumanizeYearMonthDay = (dueDate) => {
+  if (!(dueDate instanceof Date)) {
+    return ``;
+  }
+  return moment(dueDate).format(`YY MM DD HH:mm`);
+};
+
+export const formatHumanizeFull = (dueDate) => {
+  if (!(dueDate instanceof Date)) {
+    return ``;
+  }
+  return moment(dueDate).format(`YY MM DD HH:mm`);
+};
+
+export const getDuration = (startTime, endTime) => {
+  const dateB = moment(endTime);
+  const dateC = moment(startTime);
+  const duration = dateB.diff(dateC);
+  if (duration > 1440) {
+    return moment(duration).format(`DD[D] HH[H] mm[M]`);
+  }
+  if (duration > 60) {
+    return moment(duration).format(`HH[H] mm[M]`);
+  } else {
+    return moment(duration).format(`mm[M]`);
+  }
+
 };
 
 // Функция помещает задачи без даты в конце списка,
